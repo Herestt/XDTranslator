67e9a40c647b624134cd0f210d37d69b26e7b4ab
Herestt <herestt@gmail.com>
2013-07-21 17:44:40 +0200

    Initialization.

---
+++ src/com/herestt/xd/translator/ui/XDTranslator.java
@@ -0,0 +1,16 @@
+package com.herestt.xd.translator.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class XDTranslator extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
